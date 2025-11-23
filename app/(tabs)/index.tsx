eae7934a1d49e650d28d07769fb9f2283477b4f8
Patrick Benedict Lirio <86680909+PatrickLirio@users.noreply.github.com>
2025-11-23 14:51:06 +0800

    developed the navigation and the logo of the app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,16 @@
 import { Text, View } from "react-native";
-import "./global.css";
+import "../global.css";
 
 export default function Index() {
   return (
-   <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-2xl font-bold text-white">
-        Welcome to Mobile Development
+   <View className="flex-1 items-center justify-center bg-yellow-500">
+      <Text className="text-5xl font-bold text-dark-200">
+        Welcome!
       </Text>
       <Text className="text-xl text-white mt-4">
        Patrick!
       </Text>
+      
     </View>
   );
 }
